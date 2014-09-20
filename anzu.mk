6b032709d03328d124c2f1cd1cd987a303870dd0
Michael Bestas <mikeioannina@gmail.com>
2014-09-20 22:35:04 +0300

    Fix hardware info
    Change-Id: Id8eaa6ffabcd8075f96c68bb604b055c389d2af9

--- anzu.mk
+++ anzu.mk
@@ -39,8 +39,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240 \
     com.qc.hdmi_out=false
-
-# Hardware Info
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.device.rear_cam="8 MP Exmor R" \
-    ro.device.screen_res=480x854
